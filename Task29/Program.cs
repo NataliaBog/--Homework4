e298c4c9a7afc93f82cb8fd616ab4cd8767eff8b
Natalia <natashaaa60802@rambler.ru>
2022-08-21 16:39:42 +0500

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// программa, которая задаёт массив из N элементов 
+// и выводит их на экран
+
+Console.Clear();
+
+int[] bigarray(int[] array)
+{
+    for(int i=0; i < array.Length; i++)
+    {
+    Console.Write("Введите число ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void Display(int[] array)
+{
+    Console.Write("[ ");
+
+    for (int j = 0; j<array.Length-1; j++)
+        {
+            Console.Write(array[j] + ", ");
+        }
+
+    Console.Write( array[array.Length-1] + " ]");
+}
+
+Console.Write("Сколько чисел в вашем массиве? ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] userarray = new int [length];
+
+bigarray(userarray);
+Display(userarray);
